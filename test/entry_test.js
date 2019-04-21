b7288a3b408c1ce7d653438f47c4ed386f5215d6
Eemeli Aro <eemeli@gmail.com>
2019-04-21 15:25:00 +0300

    Copy & adapt entry_test.js from fluent-syntax/test/ in projectfluent/fluent.js@326b6ee0

---
+++ test/entry_test.js
@@ -0,0 +1,38 @@
+import assert from "assert";
+import { ftl } from "./util";
+
+import { FluentSerializer } from "../src/serializer";
+
+
+suite("Serialize entry", function() {
+  setup(function() {
+    this.serializer = new FluentSerializer();
+  });
+
+  test("simple message", function() {
+    const input = {
+      "comment": null,
+      "value": {
+        "elements": [
+          {
+            "type": "TextElement",
+            "value": "Foo"
+          }
+        ],
+        "type": "Pattern"
+      },
+      "attributes": [],
+      "type": "Message",
+      "id": {
+        "type": "Identifier",
+        "name": "foo"
+      }
+    };
+    const output = ftl`
+      foo = Foo
+    `;
+
+    const message = this.serializer.serializeEntry(input)
+    assert.deepEqual(message, output)
+  });
+});
