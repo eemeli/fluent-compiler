9933c83f91528837dcbe35eda00fdfcafb07f9db
Eemeli Aro <eemeli@gmail.com>
2019-04-22 19:43:01 +0300

    Apply Prettier style to select_expressions_test.js

--- test/select_expressions_test.js
+++ test/select_expressions_test.js
@@ -1,138 +1,137 @@
-"use strict";
-import assert from "assert";
-import FluentBundle from "../src/bundle";
-import {ftl} from "../src/util";
+'use strict'
+import assert from 'assert'
+import FluentBundle from '../src/bundle'
+import { ftl } from '../src/util'
 
-suite("Select expressions", function() {
-  let bundle, errs;
+suite('Select expressions', function() {
+  let bundle, errs
 
   setup(function() {
-    bundle = new FluentBundle("en-US", {useIsolating: false});
-    errs = [];
-  });
+    bundle = new FluentBundle('en-US', { useIsolating: false })
+    errs = []
+  })
 
-  test("missing selector", function() {
+  test('missing selector', function() {
     bundle.addMessages(ftl`
       select = {$none ->
           [a] A
          *[b] B
       }
-    `);
-    const msg = bundle.getMessage("select");
-    const val = bundle.format(msg, null, errs);
-    assert.equal(val, "B");
-    assert.equal(errs.length, 1);
-    assert(errs[0] instanceof ReferenceError); // unknown variable
-  });
+    `)
+    const msg = bundle.getMessage('select')
+    const val = bundle.format(msg, null, errs)
+    assert.equal(val, 'B')
+    assert.equal(errs.length, 1)
+    assert(errs[0] instanceof ReferenceError) // unknown variable
+  })
 
-  suite("string selectors", function() {
-    test("matching selector", function() {
+  suite('string selectors', function() {
+    test('matching selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [a] A
            *[b] B
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: "a"}, errs);
-      assert.equal(val, "A");
-      assert.equal(errs.length, 0);
-    });
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 'a' }, errs)
+      assert.equal(val, 'A')
+      assert.equal(errs.length, 0)
+    })
 
-    test("non-matching selector", function() {
+    test('non-matching selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [a] A
            *[b] B
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: "c"}, errs);
-      assert.equal(val, "B");
-      assert.equal(errs.length, 0);
-    });
-  });
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 'c' }, errs)
+      assert.equal(val, 'B')
+      assert.equal(errs.length, 0)
+    })
+  })
 
-  suite("number selectors", function() {
-    test("matching selector", function() {
+  suite('number selectors', function() {
+    test('matching selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [0] A
            *[1] B
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: 0}, errs);
-      assert.equal(val, "A");
-      assert.equal(errs.length, 0);
-    });
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 0 }, errs)
+      assert.equal(val, 'A')
+      assert.equal(errs.length, 0)
+    })
 
-    test("non-matching selector", function() {
+    test('non-matching selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [0] A
            *[1] B
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: 2}, errs);
-      assert.equal(val, "B");
-      assert.equal(errs.length, 0);
-    });
-  });
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 2 }, errs)
+      assert.equal(val, 'B')
+      assert.equal(errs.length, 0)
+    })
+  })
 
-  suite("plural categories", function() {
-    test("matching number selector", function() {
+  suite('plural categories', function() {
+    test('matching number selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [one] A
            *[other] B
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: 1}, errs);
-      assert.equal(val, "A");
-      assert.equal(errs.length, 0);
-    });
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 1 }, errs)
+      assert.equal(val, 'A')
+      assert.equal(errs.length, 0)
+    })
 
-    test("matching string selector", function() {
+    test('matching string selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [one] A
            *[other] B
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: "one"}, errs);
-      assert.equal(val, "A");
-      assert.equal(errs.length, 0);
-    });
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 'one' }, errs)
+      assert.equal(val, 'A')
+      assert.equal(errs.length, 0)
+    })
 
-    test("non-matching number selector", function() {
+    test('non-matching number selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [one] A
            *[default] D
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: 2}, errs);
-      assert.equal(val, "D");
-      assert.equal(errs.length, 0);
-    });
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 2 }, errs)
+      assert.equal(val, 'D')
+      assert.equal(errs.length, 0)
+    })
 
-    test("non-matching string selector", function() {
+    test('non-matching string selector', function() {
       bundle.addMessages(ftl`
         select = {$selector ->
             [one] A
            *[default] D
         }
-      `);
-      const msg = bundle.getMessage("select");
-      const val = bundle.format(msg, {selector: "other"}, errs);
-      assert.equal(val, "D");
-      assert.equal(errs.length, 0);
-    });
-  });
-
-});
+      `)
+      const msg = bundle.getMessage('select')
+      const val = bundle.format(msg, { selector: 'other' }, errs)
+      assert.equal(val, 'D')
+      assert.equal(errs.length, 0)
+    })
+  })
+})
