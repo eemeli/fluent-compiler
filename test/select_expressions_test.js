ff8d03f82de71c72f5b58e380a56129bb26a920e
Eemeli Aro <eemeli@gmail.com>
2019-04-22 19:41:58 +0300

    Copy select_expressions_test.js from fluent/test/ in projectfluent/fluent.js@326b6ee0

---
+++ test/select_expressions_test.js
@@ -0,0 +1,138 @@
+"use strict";
+import assert from "assert";
+import FluentBundle from "../src/bundle";
+import {ftl} from "../src/util";
+
+suite("Select expressions", function() {
+  let bundle, errs;
+
+  setup(function() {
+    bundle = new FluentBundle("en-US", {useIsolating: false});
+    errs = [];
+  });
+
+  test("missing selector", function() {
+    bundle.addMessages(ftl`
+      select = {$none ->
+          [a] A
+         *[b] B
+      }
+    `);
+    const msg = bundle.getMessage("select");
+    const val = bundle.format(msg, null, errs);
+    assert.equal(val, "B");
+    assert.equal(errs.length, 1);
+    assert(errs[0] instanceof ReferenceError); // unknown variable
+  });
+
+  suite("string selectors", function() {
+    test("matching selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [a] A
+           *[b] B
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: "a"}, errs);
+      assert.equal(val, "A");
+      assert.equal(errs.length, 0);
+    });
+
+    test("non-matching selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [a] A
+           *[b] B
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: "c"}, errs);
+      assert.equal(val, "B");
+      assert.equal(errs.length, 0);
+    });
+  });
+
+  suite("number selectors", function() {
+    test("matching selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [0] A
+           *[1] B
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: 0}, errs);
+      assert.equal(val, "A");
+      assert.equal(errs.length, 0);
+    });
+
+    test("non-matching selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [0] A
+           *[1] B
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: 2}, errs);
+      assert.equal(val, "B");
+      assert.equal(errs.length, 0);
+    });
+  });
+
+  suite("plural categories", function() {
+    test("matching number selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [one] A
+           *[other] B
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: 1}, errs);
+      assert.equal(val, "A");
+      assert.equal(errs.length, 0);
+    });
+
+    test("matching string selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [one] A
+           *[other] B
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: "one"}, errs);
+      assert.equal(val, "A");
+      assert.equal(errs.length, 0);
+    });
+
+    test("non-matching number selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [one] A
+           *[default] D
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: 2}, errs);
+      assert.equal(val, "D");
+      assert.equal(errs.length, 0);
+    });
+
+    test("non-matching string selector", function() {
+      bundle.addMessages(ftl`
+        select = {$selector ->
+            [one] A
+           *[default] D
+        }
+      `);
+      const msg = bundle.getMessage("select");
+      const val = bundle.format(msg, {selector: "other"}, errs);
+      assert.equal(val, "D");
+      assert.equal(errs.length, 0);
+    });
+  });
+
+});
