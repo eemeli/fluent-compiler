cba10d30541b1c3ac8a1cb8fc6ac714133d9b99c
Eemeli Aro <eemeli@gmail.com>
2019-04-25 18:45:13 +0300

    Adapt context_test.js

--- test/context_test.js
+++ test/context_test.js
@@ -1,157 +1,72 @@
 'use strict'
-
 import assert from 'assert'
-
-import FluentBundle from '../src/bundle'
-import FluentResource from '../src/resource'
-import { ftl } from '../src/util'
+import { compileAndRequire, ftl } from './util'
 
 suite('Bundle', function() {
-  let bundle, args, errs
-
-  setup(function() {
-    errs = []
-  })
-
-  suite('addMessages', function() {
-    suiteSetup(function() {
-      bundle = new FluentBundle('en-US', { useIsolating: false })
-      bundle.addMessages(ftl`
-        foo = Foo
-        -bar = Private Bar
-      `)
-    })
-
-    test('adds messages', function() {
-      assert.equal(bundle._messages.has('foo'), true)
-      assert.equal(bundle._terms.has('foo'), false)
-      assert.equal(bundle._messages.has('-bar'), false)
-      assert.equal(bundle._terms.has('-bar'), true)
-    })
-
-    test('preserves existing messages when new are added', function() {
-      bundle.addMessages(ftl`
-        baz = Baz
-      `)
-
-      assert.equal(bundle._messages.has('foo'), true)
-      assert.equal(bundle._terms.has('foo'), false)
-      assert.equal(bundle._messages.has('-bar'), false)
-      assert.equal(bundle._terms.has('-bar'), true)
-
-      assert.equal(bundle._messages.has('baz'), true)
-      assert.equal(bundle._terms.has('baz'), false)
-    })
-
-    test('messages and terms can share the same name', function() {
-      bundle.addMessages(ftl`
-        -foo = Private Foo
-      `)
-      assert.equal(bundle._messages.has('foo'), true)
-      assert.equal(bundle._terms.has('foo'), false)
-      assert.equal(bundle._messages.has('-foo'), false)
-      assert.equal(bundle._terms.has('-foo'), true)
-    })
-
-    test('does not overwrite existing messages if the ids are the same', function() {
-      const errors = bundle.addMessages(ftl`
-        foo = New Foo
-      `)
-
-      // Attempt to overwrite error reported
-      assert.equal(errors.length, 1)
-
-      assert.equal(bundle._messages.size, 2)
-
-      const msg = bundle.getMessage('foo')
-      const val = bundle.format(msg, args, errs)
-      assert.equal(val, 'Foo')
-      assert.equal(errs.length, 0)
-    })
-
-    test('overwrites existing messages if the ids are the same and allowOverrides is true', function() {
-      const errors = bundle.addMessages(
-        ftl`
-        foo = New Foo
-      `,
-        { allowOverrides: true }
-      )
-
-      // No overwrite errors reported
-      assert.equal(errors.length, 0)
-
-      assert.equal(bundle._messages.size, 2)
-
-      const msg = bundle.getMessage('foo')
-      const val = bundle.format(msg, args, errs)
-      assert.equal(val, 'New Foo')
-      assert.equal(errs.length, 0)
-    })
-  })
+  let bundle
 
   suite('addResource', function() {
-    suiteSetup(function() {
-      bundle = new FluentBundle('en-US', { useIsolating: false })
-      let resource = FluentResource.fromString(ftl`
-        foo = Foo
-        -bar = Bar
-      `)
-      bundle.addResource(resource)
+    suiteSetup(async function() {
+      bundle = await compileAndRequire(
+        'en-US',
+        ftl`
+          foo = Foo
+          -bar = Bar
+        `
+      )
     })
 
     test('adds messages', function() {
-      assert.equal(bundle._messages.has('foo'), true)
-      assert.equal(bundle._terms.has('foo'), false)
-      assert.equal(bundle._messages.has('-bar'), false)
-      assert.equal(bundle._terms.has('-bar'), true)
+      assert.equal(new Map(bundle.messages).has('foo'), true)
+      assert.equal(new Map(bundle.messages).has('-bar'), false)
     })
   })
 
   suite('allowOverrides', function() {
-    suiteSetup(function() {
-      bundle = new FluentBundle('en-US', { useIsolating: false })
-      let resource1 = FluentResource.fromString('key = Foo')
-      bundle.addResource(resource1)
+    suiteSetup(async function() {
+      bundle = await compileAndRequire('en-US', 'key = Foo')
     })
 
-    test('addResource allowOverrides is false', function() {
-      let resource2 = FluentResource.fromString('key = Bar')
-      let errors = bundle.addResource(resource2)
+    test('addResource allowOverrides is false', async function() {
+      let bundle2 = await compileAndRequire('en-US', 'key = Bar')
+      let errors = bundle.addResource(bundle2.messages)
       assert.equal(errors.length, 1)
-      let msg = bundle.getMessage('key')
-      assert.equal(bundle.format(msg), 'Foo')
+      assert.equal(bundle.format('key'), 'Foo')
     })
 
-    test('addResource allowOverrides is true', function() {
-      let resource2 = FluentResource.fromString('key = Bar')
-      let errors = bundle.addResource(resource2, { allowOverrides: true })
+    test('addResource allowOverrides is true', async function() {
+      let bundle2 = await compileAndRequire('en-US', 'key = Bar')
+      let errors = bundle.addResource(bundle2.messages, {
+        allowOverrides: true
+      })
       assert.equal(errors.length, 0)
-      let msg = bundle.getMessage('key')
-      assert.equal(bundle.format(msg), 'Bar')
+      assert.equal(bundle.format('key'), 'Bar')
     })
   })
 
   suite('hasMessage', function() {
-    suiteSetup(function() {
-      bundle = new FluentBundle('en-US', { useIsolating: false })
-      bundle.addMessages(ftl`
-        foo = Foo
-        bar =
-            .attr = Bar Attr
-        -term = Term
+    suiteSetup(async function() {
+      bundle = await compileAndRequire(
+        'en-US',
+        ftl`
+          foo = Foo
+          bar =
+              .attr = Bar Attr
+          -term = Term
 
-        # ERROR No value.
-        err1 =
-        # ERROR Broken value.
-        err2 = {}
-        # ERROR No attribute value.
-        err3 =
-            .attr =
-        # ERROR Broken attribute value.
-        err4 =
-            .attr1 = Attr
-            .attr2 = {}
-      `)
+          # ERROR No value.
+          err1 =
+          # ERROR Broken value.
+          err2 = {}
+          # ERROR No attribute value.
+          err3 =
+              .attr =
+          # ERROR Broken attribute value.
+          err4 =
+              .attr1 = Attr
+              .attr2 = {}
+        `
+      )
     })
 
     test('returns true only for public messages', function() {
@@ -169,26 +84,6 @@ suite('Bundle', function() {
       assert.equal(bundle.hasMessage('err2'), false)
       assert.equal(bundle.hasMessage('err3'), false)
       assert.equal(bundle.hasMessage('err4'), false)
-    })
-  })
-
-  suite('getMessage', function() {
-    suiteSetup(function() {
-      bundle = new FluentBundle('en-US', { useIsolating: false })
-      bundle.addMessages(ftl`
-        foo = Foo
-        -bar = Bar
-      `)
-    })
-
-    test('returns public messages', function() {
-      assert.equal(bundle.getMessage('foo'), 'Foo')
-    })
-
-    test('returns null for terms and missing messages', function() {
-      assert.equal(bundle.getMessage('-bar'), null)
-      assert.equal(bundle.getMessage('baz'), null)
-      assert.equal(bundle.getMessage('-baz'), null)
     })
   })
 })
