fe496058951c6ef6c76d6342d5b9e9167c32bc05
Eemeli Aro <eemeli@gmail.com>
2019-04-26 11:21:18 +0300

    Adapt isolating_test.js

--- test/isolating_test.js
+++ test/isolating_test.js
@@ -1,9 +1,6 @@
 'use strict'
-
 import assert from 'assert'
-
-import FluentBundle from '../src/bundle'
-import { ftl } from '../src/util'
+import { compileAndRequire, ftl } from './util'
 
 // Unicode bidi isolation characters.
 const FSI = '\u2068'
@@ -12,14 +9,16 @@ const PDI = '\u2069'
 suite('Isolating interpolations', function() {
   let bundle, args, errs
 
-  suiteSetup(function() {
-    bundle = new FluentBundle('en-US')
-    bundle.addMessages(ftl`
-      foo = Foo
-      bar = { foo } Bar
-      baz = { $arg } Baz
-      qux = { bar } { baz }
-    `)
+  suiteSetup(async function() {
+    bundle = await compileAndRequire(
+      'en-US',
+      ftl`
+        foo = Foo
+        bar = { foo } Bar
+        baz = { $arg } Baz
+        qux = { bar } { baz }
+      `
+    )
   })
 
   setup(function() {
@@ -27,22 +26,19 @@ suite('Isolating interpolations', function() {
   })
 
   test('isolates interpolated message references', function() {
-    const msg = bundle.getMessage('bar')
-    const val = bundle.format(msg, args, errs)
+    const val = bundle.format('bar', args, errs)
     assert.equal(val, `${FSI}Foo${PDI} Bar`)
     assert.equal(errs.length, 0)
   })
 
   test('isolates interpolated string-typed variables', function() {
-    const msg = bundle.getMessage('baz')
-    const val = bundle.format(msg, { arg: 'Arg' }, errs)
+    const val = bundle.format('baz', { arg: 'Arg' }, errs)
     assert.equal(val, `${FSI}Arg${PDI} Baz`)
     assert.equal(errs.length, 0)
   })
 
   test('isolates interpolated number-typed variables', function() {
-    const msg = bundle.getMessage('baz')
-    const val = bundle.format(msg, { arg: 1 }, errs)
+    const val = bundle.format('baz', { arg: 1 }, errs)
     assert.equal(val, `${FSI}1${PDI} Baz`)
     assert.equal(errs.length, 0)
   })
@@ -51,16 +47,14 @@ suite('Isolating interpolations', function() {
     const dtf = new Intl.DateTimeFormat('en-US')
     const arg = new Date('2016-09-29')
 
-    const msg = bundle.getMessage('baz')
-    const val = bundle.format(msg, { arg }, errs)
+    const val = bundle.format('baz', { arg }, errs)
     // format the date argument to account for the testrunner's timezone
     assert.equal(val, `${FSI}${dtf.format(arg)}${PDI} Baz`)
     assert.equal(errs.length, 0)
   })
 
   test('isolates complex interpolations', function() {
-    const msg = bundle.getMessage('qux')
-    const val = bundle.format(msg, { arg: 'Arg' }, errs)
+    const val = bundle.format('qux', { arg: 'Arg' }, errs)
 
     const expected_bar = `${FSI}${FSI}Foo${PDI} Bar${PDI}`
     const expected_baz = `${FSI}${FSI}Arg${PDI} Baz${PDI}`
@@ -72,12 +66,14 @@ suite('Isolating interpolations', function() {
 suite('Skip isolation cases', function() {
   let bundle, args, errs
 
-  suiteSetup(function() {
-    bundle = new FluentBundle('en-US')
-    bundle.addMessages(ftl`
-      -brand-short-name = Amaya
-      foo = { -brand-short-name }
-    `)
+  suiteSetup(async function() {
+    bundle = await compileAndRequire(
+      'en-US',
+      ftl`
+        -brand-short-name = Amaya
+        foo = { -brand-short-name }
+      `
+    )
   })
 
   setup(function() {
@@ -85,8 +81,7 @@ suite('Skip isolation cases', function() {
   })
 
   test('skips isolation if the only element is a placeable', function() {
-    const msg = bundle.getMessage('foo')
-    const val = bundle.format(msg, args, errs)
+    const val = bundle.format('foo', args, errs)
     assert.equal(val, `Amaya`)
     assert.equal(errs.length, 0)
   })
